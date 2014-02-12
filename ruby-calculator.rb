52f67f399f9636bb0cbbf1c3bfe7c5e204e2ba4e
Sailesh Patel <saileshapatel@gmail.com>
2014-02-12 06:38:44 +0000

    Changed integers in to floating point!!!
    I changed the integers in to floating point in order for Ruby to return
    decimal answers.

--- ruby-calculator.rb
+++ ruby-calculator.rb
@@ -1,42 +1,63 @@
+# created function, which takes two variables 
 def addition(a, b)
-	return a.to_i + b.to_i
+	# 'a' and 'b' are converted in to floating point from strings and added together
+	return a.to_f + b.to_f
 end
 
+# created function, which takes two variables
 def subtraction(a, b)
-	return a.to_i - b.to_i
+	# 'a' and 'b' are converted in to floating point from strings and subtracted
+	return a.to_f - b.to_f
 end
 
+# created function, which takes two variables
 def multiplication(a, b)
-	return a.to_i * b.to_i
+	# 'a' and 'b' are converted in to floating point from string and multiplied
+	return a.to_f * b.to_f
 end 
 
+# created function, which takes two variables
 def division(a, b)
-	return a.to_i / b.to_i
+	# 'a' and 'b' are converted in to floating point from strings and divided
+	return a.to_f / b.to_f
 end 
 
-
+# creating variable and assigned the boolean operator of 'true' to it.
+# this helps us later on, when it comes to looping.
 keepProgramRunning = true;
 
+# creating a 'while' statement, which creates a loop.
 while keepProgramRunning
 
-	puts "Please choose out of the following options: "
+	# printing a string
 
+	puts "Please choose out of the following options: "
+	puts "\n"
+	# printing a string
 	puts "1: Addition"
 	puts "2: Subtraction"
 	puts "3: Multiplication"
 	puts "4: Division"
 	puts "5: Exit Program"
 
+	# takes user input and assigns it to the variable 'choice'
 	choice = gets.chomp()
 
-
+	# if user inputs '1' then the following happens:
 	if choice == "1"
+		# printing string
 		puts "Please enter the value of A: "
+		# takes user input and assigns it to the variable 'a'
 		a = gets.chomp()
+		# printing string
 		puts "Please enter the value of B: "
+		# takes user input and assigns it to the variable 'b'
 		b = gets.chomp()
+		# printing string
 		puts "Here's your answer!\n"
+		# printing function 'addition'
 		puts addition(a,b)
+		# printing string with formatter
 		puts "\n"
 
 	elsif choice == "2"
