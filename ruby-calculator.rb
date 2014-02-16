8b85b3ea2a8328c11dcfeb16a27fb43008fe8459
Sailesh Patel <saileshapatel@gmail.com>
2014-02-16 16:29:01 +0000

    Added additional comments
    Added more comments to aid understanding.

--- ruby-calculator.rb
+++ ruby-calculator.rb
@@ -60,42 +60,71 @@ while keepProgramRunning
 		# printing string with formatter
 		puts "\n"
 
+	# if user inputs '2' then the following happens:
 	elsif choice == "2"
+		# printing string
 		puts "Please enter the value of A: "
+		# takes user input and assigns it to the variable 'a'
 		a = gets.chomp()
+		# printing string
 		puts "Please enter the value of B: "
+		# takes user input and assigns it to the variable 'b'
 		b = gets.chomp()
+		# printing string
 		puts "Here's your answer!\n"
+		# printing function 'subtractions'
 		puts subtraction(a,b)
 		puts "\n"
 
+	# if user inputs '3' then the following happens:
 	elsif choice == "3"
+		# printing string
 		puts "Please enter the value of A: "
+		# takes user input and assigns it to the variable 'a'
 		a = gets.chomp()
+		# printing string
 		puts "Please enter the value of B: "
+		# takes user input and assigns it to the variable 'b'
 		b = gets.chomp()
+		# printing string
 		puts "Here's your answer!\n"
+		# printing function 'multiplication'
 		puts multiplication(a,b)
+		puts "\n"
 
+	# if user inputs '4' then the following happens:
 	elsif choice == "4"
+		# printing string
 		puts "Please enter the value of A: "
+		# takes user input and assigns it to the variable 'a'
 		a = gets.chomp()
+		# printing string
 		puts "Please enter the value of B: "
+		# takes user input and assigns it to the variable 'b'
 		b = gets.chomp()
+		# printing string
 		puts "Here's your answer!\n"
+		# printing function 'division'
 		puts division(a,b)
-		
+		puts "\n"
 
+	# if user inputs '5' then the following happens:
 	elsif choice == "5"
+		# printing string
 		puts "Closing program!"
+		# assigns value 'false' to variable 'keepProgramRunning' which ends the program by ending the loop
 		keepProgramRunning = false;
 		
+	# if user inputs other value
 	else
+		# printing string
 		puts "Not a valid option.\n"
+	
+	# ends 'if elsif' loop
 	end
 
 
-	
+# ends 'while' loop	
 end
 
 
